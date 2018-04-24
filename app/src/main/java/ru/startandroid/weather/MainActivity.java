f47183d868cf490e08553eb757a8348879ec9d92
sher <Sherzodbek3021235@gmail.com>
2018-04-24 18:01:12 +0500

    AsyncTask & OkHttp

--- app/src/main/java/ru/startandroid/weather/MainActivity.java
+++ app/src/main/java/ru/startandroid/weather/MainActivity.java
@@ -1,9 +1,6 @@
 package ru.startandroid.weather;
-import android.annotation.TargetApi;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,18 +10,20 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import ru.startandroid.weather.ImageByter;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
     Button btn;
     TextView textView, textView2, textView3;
     ImageView imgview;
     MyTask mt;
     final String LOG_TAG = "myLogs";
-    String OPEN_WEATHER_MAP_API =
+    OkHttpClient client;
+    String url =
             "http://api.openweathermap.org/data/2.5/forecast?id=1512440&APPID=a7dc109561ec63ddd24cd4df691e3043";
 
     @Override
@@ -42,38 +41,51 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 mt = new MyTask();
                 mt.execute();
-
-                Log.d(LOG_TAG, "T is started");
             }
         });
     }
-
-    class MyTask extends AsyncTask<Void, Void, Void> {
+    class MyTask extends AsyncTask<Void, Void, String> {
         String tempC, desc, name;
         Bitmap bitmap;
+        String json = null;
 
         @Override
-        protected Void doInBackground(Void... voids) {
+        protected String doInBackground(Void... voids) {
+
+
             try {
-                URL url = new URL(String.format("http://api.openweathermap.org/data/2.5/forecast?id=1512440&APPID=" +
-                        "a7dc109561ec63ddd24cd4df691e3043"));
-                HttpURLConnection connection =
-                        (HttpURLConnection) url.openConnection();
-                Log.d(LOG_TAG, "Conection is open");
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+                client = new OkHttpClient();
+                final Call call = client.newCall(request);
+                Response response = call.execute();
+                if (response.isSuccessful())
+                    json = response.body().string();
 
-                connection.addRequestProperty("a7dc109561ec63ddd24cd4df691e3043",
-                        getString(R.string.open_weather_maps_app_id));
-                Log.d(LOG_TAG, "RequestProperty");
+                Log.d("Func", json);
 
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
-                StringBuffer json = new StringBuffer(1024);
-                String tmp = "";
-                while ((tmp = reader.readLine()) != null) {
-                    json.append(tmp).append("\n");
-                }
-                reader.close();
-                Log.d(LOG_TAG, json.toString());
+
+//                URL url = new URL(String.format("http://api.openweathermap.org/data/2.5/forecast?id=1512440&APPID=" +
+//                        "a7dc109561ec63ddd24cd4df691e3043"));
+//                HttpURLConnection connection =
+//                        (HttpURLConnection) url.openConnection();
+//                Log.d(LOG_TAG, "Conection is open");
+//
+//                connection.addRequestProperty("a7dc109561ec63ddd24cd4df691e3043",
+//                        getString(R.string.open_weather_maps_app_id));
+//                Log.d(LOG_TAG, "RequestProperty");
+//
+//                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//
+//                StringBuffer json = new StringBuffer(1024);
+//                String tmp = "";
+//                while ((tmp = reader.readLine()) != null) {
+//                    json.append(tmp).append("\n");
+//                }
+//                reader.close();
+//                Log.d(LOG_TAG, json.toString());
                 JSONObject data = new JSONObject(json.toString());
                 if (data.getInt("cod") != 200) {
                     Log.d(LOG_TAG, "Returned null");
@@ -97,16 +109,15 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(LOG_TAG, weather.getString("icon"));
                 final String iconUrls = "http://openweathermap.org/img/w/" + icond + ".png";
                 Log.d(LOG_TAG, (String) weather.get("icon"));
-                connection.disconnect();
                 Log.d(LOG_TAG, "Disconnect");
                 this.bitmap = ImageByter.creatBitmap(iconUrls);
 
             } catch (Exception e) {
                 Log.e(LOG_TAG, "Exeption", e);
             }
-            return null;
+            return json;
         }
-        protected void onPostExecute(Void result) {
+        protected void onPostExecute(String result) {
             super.onPostExecute(result);
             textView.setText(tempC);
             textView2.setText(desc);
