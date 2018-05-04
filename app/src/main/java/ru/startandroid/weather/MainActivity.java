e977c231731ecb332b3955becc1254d5bb8e6cdf
sher <Sherzodbek3021235@gmail.com>
2018-05-04 19:49:35 +0500

    05052018

--- app/src/main/java/ru/startandroid/weather/MainActivity.java
+++ app/src/main/java/ru/startandroid/weather/MainActivity.java
@@ -2,25 +2,18 @@ package ru.startandroid.weather;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 import com.google.gson.Gson;
-import java.io.IOException;
-import java.util.List;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import ru.startandroid.weather.response.MainParent;
-import ru.startandroid.weather.response.Weather;
+import com.google.gson.GsonBuilder;
+
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private Button btn;
@@ -42,62 +35,71 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getAsyncCall();
+
                 Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
                 imgview.startAnimation(animation);
             }
         });
     }
+    private final String url =
+            "http://api.openweathermap.org";
+    private Gson gson = new GsonBuilder().create();
+    private Retrofit retrofit = new Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .baseUrl(url)
+            .build();
+    ResponseApi responseApi = (ResponseApi) retrofit.create(Link.class);
 
-    public void getAsyncCall() {
-        String url =
-                "http://api.openweathermap.org/data/2.5/forecast?id=1512440&APPID=a7dc109561ec63ddd24cd4df691e3043";
-        final OkHttpClient httpClient = new OkHttpClient();
-        final Request request = new Request.Builder()
-                .url(url)
-                .build();
-        final Gson gson = new Gson();
-        httpClient.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Toast.makeText(MainActivity.this, "Something went wrong. Check logs", Toast.LENGTH_SHORT).show();
-                Log.e(LOG_TAG, "Error: ", e);
-            }
 
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                String body = response.body().string();
-                Log.d(LOG_TAG, body);
-                final ResponseApi responseApi = gson.fromJson(body, ResponseApi.class);
-                Log.d(LOG_TAG, responseApi.toString());
-                if (!response.isSuccessful()) {
-                    throw new IOException("Error response " + response);
-                }
-                final MainParent temp = responseApi.list.get(0);
-                List<Weather> weatherList = temp.getWeatherList();
-                int tempk = (int) temp.getMain().getTemp();
-                final int temps = (tempk - 273);
-                final String city = responseApi.getCity().getName();
-                final String main = weatherList.get(0).getMain();
-                if (weatherList != null && !weatherList.isEmpty()) {
-                    String icon = weatherList.get(0).getIcon();
-                    if (!TextUtils.isEmpty(icon)) {
-                        final String iconUrls = "http://openweathermap.org/img/w/" + icon + ".png";
-                        bitmap = ImageByter.creatBitmap(iconUrls);
-                    }
-                }
-                MainActivity.this.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        textView.setText(String.valueOf(temps) + "°C");
-                        textView2.setText(main);
-                        textView3.setText(city);
-                        if (bitmap != null) {
-                            imgview.setImageBitmap(bitmap);
-                        }
-                    }
-                });
-            }
-        });
-    }
+    //    public void getAsyncCall() {
+//        String url =
+//                "http://api.openweathermap.org/data/2.5/forecast?id=1512440&APPID=a7dc109561ec63ddd24cd4df691e3043";
+//        final OkHttpClient httpClient = new OkHttpClient();
+//        final Request request = new Request.Builder()
+//                .url(url)
+//                .build();
+//        final Gson gson = new Gson();
+//        httpClient.newCall(request).enqueue(new Callback() {
+//            @Override
+//            public void onFailure(Call call, IOException e) {
+//                Toast.makeText(MainActivity.this, "Something went wrong. Check logs", Toast.LENGTH_SHORT).show();
+//                Log.e(LOG_TAG, "Error: ", e);
+//            }
+//
+//            @Override
+//            public void onResponse(Call call, Response response) throws IOException {
+//                String body = response.body().string();
+//                Log.d(LOG_TAG, body);
+//                final ResponseApi responseApi = gson.fromJson(body, ResponseApi.class);
+//                Log.d(LOG_TAG, responseApi.toString());
+//                if (!response.isSuccessful()) {
+//                    throw new IOException("Error response " + response);
+//                }
+//                final MainParent temp = responseApi.list.get(0);
+//                List<Weather> weatherList = temp.getWeatherList();
+//                int tempk = (int) temp.getMain().getTemp();
+//                final int temps = (tempk - 273);
+//                final String city = responseApi.getCity().getName();
+//                final String main = weatherList.get(0).getMain();
+//                if (weatherList != null && !weatherList.isEmpty()) {
+//                    String icon = weatherList.get(0).getIcon();
+//                    if (!TextUtils.isEmpty(icon)) {
+//                        final String iconUrls = "http://openweathermap.org/img/w/" + icon + ".png";
+//                        bitmap = ImageByter.creatBitmap(iconUrls);
+//                    }
+//                }
+//                MainActivity.this.runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        textView.setText(String.valueOf(temps) + "°C");
+//                        textView2.setText(main);
+//                        textView3.setText(city);
+//                        if (bitmap != null) {
+//                            imgview.setImageBitmap(bitmap);
+//                        }
+//                    }
+//                });
+//            }
+//        });
+//    }
 }
